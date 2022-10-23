067ed194af4252488756f47f613a6387f55d6c8f
PoustniGaara <viktortindula@gmail.com>
2022-10-23 10:05:45 +0200

    Update after jakob's advice

---
+++ AppMVCDataAccess/IProductDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using AppMVCClassLibrary;
+
+namespace DataAccessLayer
+{
+    public class IProductDataAccess
+    {
+        public interface IMovieDataAccess
+        {
+            public IEnumerable<Product> GetAll();
+            public Product GetProductById(int id);
+            public Product GetProductByCategory(Category category);
+            public int AddProduct(Product product);
+            public bool DeleteProduct(int id);
+            public bool UpdateProduct(Product product);
+        }
+    }
+}
---
+++ WebAppMVC/Controllers/ProductController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiClient.DTOs;
+
+namespace WebAppMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Product product)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Product product)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
