cca32fe84d0346fd943a47f7824c92b8c3d01574
ppastierova <93515393+ppastierova@users.noreply.github.com>
2022-11-08 13:27:18 +0100

    Update OrderProfile.cs

--- WebAPI/DtoProfiles/OrderProfile.cs
+++ WebAPI/DtoProfiles/OrderProfile.cs
@@ -11,7 +11,7 @@ namespace WebApi.DtoProfiles
         {
             //OrderStatus and User are complex types so, map them to simple type using for member
             CreateMap<Order, OrderDto>()
-                    .ForMember(dest => dest.Status, act => act.MapFrom(src => src.Status.Status))
+                    .ForMember(dest => dest.Status, act => act.MapFrom(src => src.Status))
                     .ForMember(dest => dest.UserEmail, act => act.MapFrom(src => src.User.Email))
                     .ReverseMap();
         }
