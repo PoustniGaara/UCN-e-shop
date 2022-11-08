0a60f17bbcbedbab403f156beb2d3205883c936c
MatejRolko <10407572@ucn.dk>
2022-11-08 13:11:42 +0100

    DAO

---
+++ AppMVCDataAccess/IOrderDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Model;
+
+namespace DataAccessLayer
+{
+    public interface IOrderDataAccess
+    {
+        public Task<IEnumerable<Order>> GetAllAsync();
+        public Task<Order> GetOrderByIdAsync(int id);
+        public Task<Order> GetOrderByUserAsync(User user);
+        public Task<int> CreateOrderAsync(Order order);
+        public Task<bool> DeleteOrderAsync(int id);
+        public Task<bool> UpdateOrderAsync(Order order);
+    }
+}
