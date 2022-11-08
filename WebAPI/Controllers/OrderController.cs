4e226b966b87748760c05ecdd56f0aa04a135eb5
MatejRolko <10407572@ucn.dk>
2022-11-08 13:35:15 +0100

    Create OrderController.cs

---
+++ WebAPI/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+    #region Properties and Constructor
+    IOrderDataAccess _orderDataAccess;
+    private readonly IMapper _mapper;
+
+    public OrderController(IOrderDataAccess orderDataAccess, IMapper mapper)
+    {
+        _orderDataAccess = orderDataAccess;
+        _mapper = mapper;
+    }
+
+        #endregion
+    }
+}
