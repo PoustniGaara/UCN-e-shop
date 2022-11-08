c533c184716fb16b0c41f241e65f51747319d152
ppastierova <93515393+ppastierova@users.noreply.github.com>
2022-11-08 11:42:38 +0100

    Model classes

---
+++ AppMVCDataAccess/Model/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace DataAccessLayer.Model
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalPrice { get; set; }
+        public enum Status { get; set; }
+        public string Note { get; set; }
+
+      
+    }
+
+    public enum Status
+    {
+        CART, PLACED
+    }
+}
+
---
+++ AppMVCDataAccess/Model/Payment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace DataAccessLayer.Model
+{
+    public class Payment
+    {
+        public double Total { get; set; }
+        public DateTime Date { get; set; }
+        public int Id { get; set; }
+
+    }
+}
+
