e4694645271c94e84d3b05b75590632572766919
MatejRolko <10407572@ucn.dk>
2022-11-08 14:29:46 +0100

    Order DAO

--- AppMVCDataAccess/IOrderDataAccess.cs
+++ AppMVCDataAccess/IOrderDataAccess.cs
@@ -6,7 +6,7 @@ namespace DataAccessLayer
     {
         public Task<IEnumerable<Order>> GetAllAsync();
         public Task<Order> GetOrderByIdAsync(int id);
-        public Task<Order> GetOrderByUserAsync(User user);
+        public Task<IEnumerable<Order>> GetOrdersByUserAsync(User user);
         public Task<int> CreateOrderAsync(Order order);
         public Task<bool> DeleteOrderAsync(int id);
         public Task<bool> UpdateOrderAsync(Order order);
--- AppMVCDataAccess/SqlDbDataAccess/OrderDataAccessDAO.cs
+++ AppMVCDataAccess/SqlDbDataAccess/OrderDataAccessDAO.cs
@@ -1,4 +1,7 @@
 ﻿using DataAccessLayer.Model;
+using System.Security.Principal;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace DataAccessLayer.SqlDbDataAccess
 {
@@ -7,51 +10,188 @@ namespace DataAccessLayer.SqlDbDataAccess
         private string connectionstring;
 
         public OrderDataAccessDAO(string connectionstring)
-        {
+        { 
             this.connectionstring = connectionstring;
         }
 
-        public Task<int> CreateOrderAsync(Order order)
+        public async Task<int> CreateOrderAsync(Order order)
         {
+            int id = -1;
+            using SqlConnection connection = new SqlConnection(connectionstring);
             try
             {
-                // SQL statement
+                connection.Open();
+                string queryString = "INSERT INTO dbo.Order ('orderStatus', 'customer') VALUES (@status, @customer); SELECT CAST(scope_identity() AS int)";
+                SqlCommand insertCommand = new SqlCommand(queryString, connection);
+                insertCommand.Parameters.AddWithValue("@status", order.Status);
+                insertCommand.Parameters.AddWithValue("@customer", order.User.Email);
+                id = (int)insertCommand.ExecuteScalar();
+                order.Id = id;
             }
-            catch (Exception e)
+            catch (SqlException sqlex)
             {
-                throw new Exception("message", e);
+                Console.WriteLine("An error occured while inserting account information into the database: " + sqlex);
             }
-            throw new NotImplementedException();
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occured while inserting account information into the database: " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return id;
         }
 
-        public Task<bool> DeleteOrderAsync(int id)
+        public async Task<bool> DeleteOrderAsync(int id)
         {
-            throw new NotImplementedException();
+            using SqlConnection connection = new SqlConnection(connectionstring);
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("DELETE FROM Order WHERE id = " + id, connection);
+                int affected = command.ExecuteNonQuery();
+                if (affected == 1)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occured while deleting an account: " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
         }
 
-        public Task<IEnumerable<Order>> GetAllAsync()
+        public async Task<IEnumerable<Order>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            List<Order> orders = new List<Order>();
+            using SqlConnection connection = new SqlConnection(connectionstring);
+            try
+            {
+                connection.Open();
+                SqlCommand selectCommand = new SqlCommand("Select * from Order");
+                selectCommand.Connection = connection;
+                SqlDataReader reader = selectCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    User user = UserDAO.GetByIdAsync(reader.GetString("customer"));
+                    List<LineItem> items = OrderLineItemsDAO.GetByIdAsync(reader.GetInt32("id"));
+                    orders.Add(new Order(reader.GetInt32("id"), reader.GetDateTime("date"), reader.GetDecimal("total"), (Status)reader.GetInt32("status"), reader.GetString("note"), user, items));
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                Console.WriteLine("An sql error occured while trying to retrieve all the accounts from the database: " + sqlex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unspecified error occured while trying to retrieve all the accounts from the database: " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return orders;
         }
 
-        public Task<Order> GetOrderByIdAsync(int id)
+        public async Task<Order> GetOrderByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            using SqlConnection connection = new SqlConnection(connectionstring);
+            try
+            {
+                connection.Open();
+                string query = "SELECT * FROM Order WHERE id = @id";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@id", id);
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                User user = UserDAO.GetByIdAsync(reader.GetString("customer"));
+                List<LineItem> items = OrderLineItemsDAO.GetByIdAsync(reader.GetInt32("id"));
+                return new Order(reader.GetInt32("id"), reader.GetDateTime("date"), reader.GetDecimal("total"), (Status)reader.GetInt32("status"), reader.GetString("note"), user, items);
+            }
+            catch (SqlException sqlex)
+            {
+                Console.WriteLine("An sql error occured while trying to retrieve all the accounts from the database: " + sqlex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unspecified error occured while trying to retrieve all the accounts from the database: " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return null;
         }
 
-        public Task<Order> GetOrderByUser(User user)
+        public async Task<IEnumerable<Order>> GetOrdersByUserAsync(User user)
         {
-            throw new NotImplementedException();
+            List<Order> orders = new List<Order>();
+            using SqlConnection connection = new SqlConnection(connectionstring);
+            try
+            {
+                connection.Open();
+                SqlCommand selectCommand = new SqlCommand("Select * from Order where customer = " + user.Email);
+                selectCommand.Connection = connection;
+                SqlDataReader reader = selectCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    List<LineItem> items = OrderLineItemsDAO.GetByIdAsync(reader.GetInt32("id"));
+                    orders.Add(new Order(reader.GetInt32("id"), reader.GetDateTime("date"), reader.GetDecimal("total"), (Status)reader.GetInt32("status"), reader.GetString("note"), user, items));
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                Console.WriteLine("A sql error occured while trying to retrieve all the accounts from the database: " + sqlex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unspecified error occured while trying to retrieve all the accounts from the database: " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return orders;
         }
 
-        public Task<Order> GetOrderByUserAsync(User user)
+        public async Task<bool> UpdateOrderAsync(Order order)
         {
-            throw new NotImplementedException();
-        }
+            if (order == null)
+            {
+                return false;
+                throw new NullReferenceException();
+            }
 
-        public Task<bool> UpdateOrderAsync(Order order)
-        {
-            throw new NotImplementedException();
+            using SqlConnection connection = new SqlConnection(connectionstring);
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("UPDATE Order SET date = @date, total = @total, orderStatus = @staus WHERE id = @id", connection);
+                command.Parameters.AddWithValue("@date", order.Date);
+                command.Parameters.AddWithValue("@total", order.TotalPrice);
+                command.Parameters.AddWithValue("@status", (int)order.Status);
+                command.Parameters.AddWithValue("@id", order.Id);
+                int affected = command.ExecuteNonQuery();
+                if (affected == 1)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occured while updateing name of an account: " + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
         }
     }
 }
