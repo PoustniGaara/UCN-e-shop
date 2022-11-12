cd538c38fbd2bd98ec6d55d8f4d330f18f4879cb
PoustniGaara <viktortindula@gmail.com>
2022-11-12 10:33:55 +0100

    Added new XUNIT test project

---
+++ AppMVCDataAccess/InMemoryTestDataAccess/InMemoryProductDAO.cs
@@ -0,0 +1,47 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.InMemoryTestDataAccess
+{
+    public class InMemoryProductDAO : IProductDataAccess
+    {
+        public Task<int> CreateProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProductAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetByCategoryAsync(string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Testing/Backend/Controllers/TestProductController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using DataAccessLayer;
+using DataAccessLayer.InMemoryTestDataAccess;
+using LoggerService;
+using WebApi.Controllers;
+
+namespace Testing.Backend.Controllers
+{
+    public class TestProductController
+    {
+        IProductDataAccess productDataAcces;
+        ILoggerManager logger;
+        IMapper  mapper;
+
+        public TestProductController()
+        {
+            productDataAcces = new InMemoryProductDAO();
+            logger = new LoggerManager();
+        }
+        
+        [Test]
+        public async Task Get_Products_Without_Parameter_Success()
+        {
+            //ARRANGE
+            ProductsController controller = new(productDataAcces, mapper, logger);
+            //controller.Request = new HttpRequestMessage();
+            //controller.Configuration = new HttpConfiguration();
+
+            //ACT
+            var response = controller.Get();
+            //ASSERT
+
+            //// Arrange
+            //var controller = new ProductsController(repository);
+            //controller.Request = new HttpRequestMessage();
+            //controller.Configuration = new HttpConfiguration();
+
+            //// Act
+            //var response = controller.Get(10);
+
+            //// Assert
+            //Product product;
+            //Assert.IsTrue(response.TryGetContentValue<Product>(out product));
+            //Assert.AreEqual(10, product.Id);
+
+        }
+    }
+}
