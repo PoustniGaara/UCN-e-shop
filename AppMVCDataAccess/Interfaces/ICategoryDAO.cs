05e344d859430e30d201e655b6f5c4ee2f39a656
PoustniGaara <viktortindula@gmail.com>
2022-11-21 12:21:41 +0100

    dao

---
+++ AppMVCDataAccess/Interfaces/ICategoryDAO.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Model;
+
+namespace DataAccessLayer.Interfaces
+{
+    public interface IProductDataAccess 
+    {
+        public Task<IEnumerable<Product>> GetAllAsync();
+        public Task<Product> GetByIdAsync(int id);
+        public Task<int> CreateAsync(Product model);
+        public Task DeleteAsync(int id);
+        public Task UpdateAsync(Product product);
+        public Task<IEnumerable<Product>> GetAllByCategoryAsync(string category);
+        public Task<Product> GetByCategoryAsync(string category);
+    }
+
+}
